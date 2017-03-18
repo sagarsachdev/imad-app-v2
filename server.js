d9f0c857a011486b8f79793a0b2a32fa4e15bd8f
sagarsachdev <sagarsachdev25@yahoo.com>
2017-03-18 21:03:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -134,7 +134,7 @@ app.get('/articles/:articleName',function(req,res){
     // articleName == article-one
     // articles[articleName] == {} content object for article one
     
-    pool.query("SELECT * FROM article WHERE title = " + req.params.articleName , function(err, result){
+    pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'" , function(err, result){
        if(err){
            res.ststus(500).send(err.toString());
        } else{
