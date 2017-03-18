41353c89299534176747a3d929cd74b1960ac98d
sagarsachdev <sagarsachdev25@yahoo.com>
2017-03-18 20:05:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,6 +18,7 @@ button.onclick = function(){
         }    
         //Not done yet 
     };
+    
     //Make the request
     request.open('GET','http://sagarsachdev.imad.hasura-app.io/counter',true);
     request.send(null);
