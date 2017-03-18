4f2ae711935cc8fd93cb63f2ec73290a575a914c
sagarsachdev <sagarsachdev25@yahoo.com>
2017-03-18 22:43:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -142,7 +142,7 @@ app.get('/articles/:articleName',function(req,res){
                res.status(404).send('Article Not Found.');
            }else{
                var articleData = result.rows[0];
-               res.send(createTemplate(articles[articleName]));
+               res.send(createTemplate(articleData));
            }
        }
     });
