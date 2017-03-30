8f6422a5c131ad02638f67aa63163a209a9b0a71
sagarsachdev <sagarsachdev25@yahoo.com>
2017-03-30 14:50:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
 
 var config = {
     user : 'sagarsachdev',
@@ -13,6 +14,7 @@ var config = {
 };
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 var articles = {
     'article-one' : {
@@ -97,6 +99,24 @@ return htmlTemplate;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+
+app.post('/create-user', function(req,res){
+   // username, password
+   // JSON
+   var username = req.body.username;
+   var password = req.body.password;
+   var salt = crypto.getRandomBytes(128).toString('hex');
+   var dbString = hash(password,salt);
+    pool.query('INSERT INTO "user" (username, password) VALUES ($1,$2)', [username, dbString], function(err, result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+            res.send("user successfully created: " + username);
+        }        
+    });
 });
 
 
